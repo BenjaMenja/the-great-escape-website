c6f62c5ddc73b2f80707f455c936d9f3bdad0ce5
Ben Gelinas <benjamenjalinas@gmail.com>
2023-04-03 00:14:28 -0400

    Fixed meta tag, design return button, environment page?

--- src/components/design/DesignReturn.js
+++ src/components/design/DesignReturn.js
@@ -3,7 +3,7 @@ import {Button} from "reactstrap";
 function DesignReturn() {
     return(
         <div style={{paddingTop: '3rem'}}>
-            <Button color='primary' onClick={() => {window.location.href = '/design'}}>
+            <Button color='primary' onClick={() => {window.location.href = '/the-great-escape-website/design'}}>
                 <a>Back to Design</a> <i className={'bi-arrow-right'}></i>
             </Button>
         </div>
