9b46c9ca3d6a2da12ff093493a723c57909c2b46
Ben Gelinas <benjamenjalinas@gmail.com>
2023-04-18 14:32:26 -0400

    Developer Blog Updates

--- src/components/DeveloperBlog/weeklyreport.js
+++ src/components/DeveloperBlog/weeklyreport.js
@@ -4,9 +4,27 @@ import {Col, List, Row} from "reactstrap";
 function WeeklyReport(props) {
     const techentries = props.techentries
     const artentries = props.artentries
+    const teamentries = props.teamentries
+    const hasTeamEntries = (teamentries) => {
+        if (teamentries !== undefined) {
+            if (teamentries.length > 0) {
+                return true
+            }
+        } else return false
+        return false
+    }
     return (
         <div style={{marginBottom: '4rem'}}>
             <h3 style={{marginBottom: '2rem'}}>Week: {props.dates}</h3>
+            {hasTeamEntries(teamentries) ? <>
+                <h4 style={{marginBottom: '2rem'}}>Major Team Milestones</h4>
+                <Col style={{marginLeft: '42.5vw', width: '20vw', textAlign: 'left', padding: '0px'}}>
+                    <List>
+                        {teamentries.map((heading, index) => <li key={index}>{heading}</li>)}
+                    </List>
+                </Col>
+                </>
+                : null}
             <Row>
                 <Col style={{marginLeft: '25vw', width: '25vw', textAlign: 'left', padding: '0px'}}>
                     <h3>Tech</h3>
--- src/pages/DeveloperBlog.js
+++ src/pages/DeveloperBlog.js
@@ -3,7 +3,23 @@ import WeeklyReport from "../components/DeveloperBlog/weeklyreport";
 function DeveloperBlog() {
     return(
         <div>
-            <WeeklyReport dates={'2023-4-2 - 2023-4-8'} techentries={[
+            <WeeklyReport dates={'2023-4-9 - 2023-4-15'} teamentries={[
+                'Final alpha build completed',
+                'Playtest build started',
+                'Alpha Presentation'
+            ]} techentries={[
+                'Highlight improvements',
+                'Pathfinding improvements',
+                'Controls Menu',
+                'All doors openable',
+                'Hint implementation started'
+            ]} artentries={[
+                'Beds modeled and UV\'d',
+                'Microwave and Fridge improvements'
+            ]}/>
+            <WeeklyReport dates={'2023-4-2 - 2023-4-8'} teamentries= {[
+               'First Alpha build completed'
+            ]} techentries={[
                 'Game Over screen',
                 'Win screen',
                 'Game Over conditions',
@@ -21,7 +37,6 @@ function DeveloperBlog() {
                 'Player, Mom, and Aunt modeled',
                 'Modeling and UV maps of static assets',
                 'Models for Key, Burrito, and Essential Oil Items',
-                'Bed Model',
                 'Map arrangement updates'
             ]}/>
             <WeeklyReport dates={'2023-3-26 - 2023-4-1'} techentries={[
@@ -36,7 +51,9 @@ function DeveloperBlog() {
             ]} artentries={[
                 'Houdini procedural assets for counter and shelves'
             ]}/>
-            <WeeklyReport dates={'2023-3-19 - 2023-3-25'} techentries={[
+            <WeeklyReport dates={'2023-3-19 - 2023-3-25'} teamentries={[
+                'Project Initialization'
+            ]} techentries={[
                 'Character Movement',
                 'Added initial art assets'
             ]} artentries={[
